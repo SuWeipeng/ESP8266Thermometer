4bf95e4d38fb2d92654cbca8b464f0b652f29fbf
SuWeipeng <weipeng.su@outlook.com>
2020-10-22 16:57:50 +0800

    加入 Si7051

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ i2c = I2C(scl=Pin(14), sda=Pin(12))
 
 i2c_device = i2c.scan()
 
+if 64 in i2c_device:
+    import si7051
+    si7051 = si7051.Si705x(i2c=i2c)
+    
 if 90 in i2c_device:
     sensor = mlx90614.MLX90614(i2c)
 
@@ -20,23 +24,34 @@ if 41 in i2c_device:
 
 while True:
     i2c_device = i2c.scan()
+    
+    if 90 in i2c_device:
+        t1 = sensor.read_object_temp()
+        line_2 = "temp : %.1f"%(t1)
+    else:
+        line_2 = "No sensor!!!"
+        
+    if 64 in i2c_device:
+        t2 = si7051.temperature
+        line_3 = "tmep : " + "%.1f"%(t2)
+        line_1 = "delta: " + "%.1f"%(t2-t1)
+    else:
+        line_3 = ""
+        line_1 = ""
+        
     if 41 in i2c_device:
         tof.start()
         tof.read()
-        dist_str = "dist: " + "%.1f"%(tof.read()/10) + " cm"
+        line_1 = "dist: " + "%.1f"%(tof.read()/10) + " cm"
         tof.stop()
-    else:
-        dist_str = ""
-
-    if 90 in i2c_device:
-        printstr = "temp : %.1f"%(sensor.read_object_temp())
-    else:
-        printstr = "No sensor!!!"
+    elif 64 not in i2c_device:
+        line_1 = ""
         
     if 60 in i2c_device:
         oled.fill(0)
-        oled.text(dist_str, 10, 0)
-        oled.text(printstr, 10, 16)
+        oled.text(line_1, 10, 0)
+        oled.text(line_2, 10, 15)
+        oled.text(line_3, 10, 25)
         oled.show()
     time.sleep_ms(500)
 
